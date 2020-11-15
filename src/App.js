e7e0757b12d955798b945ee5c303231ebb41f810
JSheridanMU <jdsheridan99@gmail.com>
2020-11-15 14:44:39 +0000

    002-removed some unused css and logos and changed background colour

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  document.body.style = 'background: #181717;';
   return (
     <div className="App">
       <Header/>
