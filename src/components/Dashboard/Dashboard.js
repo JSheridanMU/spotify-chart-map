69277019b5449a203ac86d287bc731882f54c0c1
JSheridanMU <jdsheridan99@gmail.com>
2020-12-29 14:35:19 +0000

    015-fixed slider vlaue hooks and added daily updating date range

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Col, Container, Row, Form, Table } from "react-bootstrap";
-import RangeSlider from 'react-bootstrap-range-slider';
+import React, { useState } from "react";
+import { Col, Container, Row, Table } from "react-bootstrap";
+import RangeSlider from "react-bootstrap-range-slider";
 import MapView from "../MapView/MapView";
-import GetDate from "../GetDate/GetDate"
 import GetCountry from "../GetCountry/GetCountry";
 //import GetCharts from "../GetCharts/GetCharts"
 //import ChartsData from "../ChartsData/ChartsData"
-import './Dashboard.css';
+import "./Dashboard.css";
+import moment from "moment";
 
 const initialCountry = {
   name: "Ireland",
@@ -14,11 +14,20 @@ const initialCountry = {
 };
 
 export default function Dashboard() {
-  const getThisYear = () => {
-    return new Date().getFullYear();
+  let dateArray = [];
+  let minDate = "01/01/2016";
+  let maxDate = moment().subtract(1, "days").format("DD/MM/YYYY"); //may need to change to subtract 2 days depending on when spotify updates
+
+  while (
+    moment(minDate, "DD/MM/YYYY").valueOf() <= moment(maxDate, "DD/MM/YYYY").valueOf()
+  ) {
+    dateArray.push(moment(minDate, "DD/MM/YYYY").format("DD/MM/YYYY"));
+    minDate = moment(minDate, "DD/MM/YYYY").add(1, "days").format("DD/MM/YYYY");
   }
-  const { date, handleDateChange } = GetDate(2020);
+
   const { country, handleInputChange } = GetCountry(initialCountry);
+  const [tempDate, setTempDate] = useState(maxDate);
+  const [finalDate, setFinalDate] = useState(tempDate);
 
   return (
     <React.Fragment>
@@ -26,29 +35,36 @@ export default function Dashboard() {
         <Row className="justify-content-md-center">
           <Col xs={6}>
             <RangeSlider
-              value={date}
+              value={tempDate}
               variant={"success"}
-              onChange={changeEvent => handleDateChange(changeEvent.target.date)}
-              min={2006}
-              max={getThisYear()}
+              onChange={(changeEvent) => setTempDate(changeEvent.target.value)}
+              onAfterChange={(changeEvent) =>
+                setFinalDate(changeEvent.target.value)
+              }
+              min={0}
+              max={dateArray.length - 1}
+              tooltip={"auto"}
+              tooltipLabel={(currentValue) => dateArray[tempDate]}
             />
           </Col>
         </Row>
         <Row>
           <Col xs={3}>
-            <h3 style={{color: "#1DB954"}}>Global Charts {date}</h3>
-            <Table className="GlobalChartTable">
-            
-            </Table>
+            <h3 style={{ color: "#1DB954" }}>
+              Global Charts{" "}
+              {dateArray[finalDate] ? dateArray[finalDate] : maxDate}
+            </h3>
+            <Table className="GlobalChartTable"></Table>
           </Col>
           <Col xs={6}>
-            <MapView handleInputChange={handleInputChange}/>
+            <MapView handleInputChange={handleInputChange} />
           </Col>
           <Col xs={3}>
-            <h3 style={{color: "#1DB954"}}>{country.name} Charts {date}</h3>
-            <Table className="CountryChartTable">
-            
-            </Table>
+            <h3 style={{ color: "#1DB954" }}>
+              {country.name} Charts{" "}
+              {dateArray[finalDate] ? dateArray[finalDate] : maxDate}
+            </h3>
+            <Table className="CountryChartTable"></Table>
           </Col>
         </Row>
       </Container>
