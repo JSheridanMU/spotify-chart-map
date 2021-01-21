967ee0c2035809468e2ae24dedee8dd925481dd1
JSheridanMU <jdsheridan99@gmail.com>
2021-01-21 16:38:38 +0000

    018-added new python script for writing JSON files to be uploaded with the Firebase CLI

--- misc scripts/ExtractSpotifyCharts.py
+++ misc scripts/ExtractSpotifyCharts.py
@@ -1,6 +1,5 @@
 from fycharts.SpotifyCharts import SpotifyCharts
 
-
 api = SpotifyCharts()
-
-api. top200Daily(output_file = "uk2.csv", start = "2017-06-01", end = "2020-12-12", region = ["gb"])
+api.top200Daily(output_file="output.csv", start="2017-01-01", end="2021-01-19", region=[
+                "dk", "gr", "is", "ch", "it", "pl", "at", "tr", "ee", "lt", "nl", "be", "es", "hu", "lu", "no", "ro", "bg", "fi", "lv", "se", "cz", "fr", "ie", "de", "gb", "sk", "global"])
